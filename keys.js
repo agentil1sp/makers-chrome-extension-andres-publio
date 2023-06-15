9e41ee9b6bbd73a70ab81a419846a08c2751be35
Andres Publio Gentile <a.gentile@correo.unimet.edu.ve>
2023-06-15 06:28:34 -0400

    new apikey

--- keys.js
+++ keys.js
@@ -1,2 +1,2 @@
-window.mykey = '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'
+window.mykey = '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'
 // window.mykey = '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'
