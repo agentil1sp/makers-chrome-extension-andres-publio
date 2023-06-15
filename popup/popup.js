91e3c3501fa1e07b7bdbbb32e72a8fd2ac05672e
Andres Publio Gentile <a.gentile@correo.unimet.edu.ve>
2023-06-15 04:08:14 -0400

    feat: Add collapsible tab and API connectivity

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,28 @@
-console.log("This is a popup!")
+function sendRequest() {
+    console.log("hi");
+    var input = document.getElementById("request").value;
+
+
+
+        //Make API request to OpenAI
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://api.openai.com/v1/chat/completions", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader("Authorization", window.mykey);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                var output = response.choices[0].message.content;
+                document.getElementById("response").innerHTML += "<p><strong>Response:</strong> " + output + "</p>";
+            }
+        };
+        var data = JSON.stringify({
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": input}]
+        });
+        xhr.send(data);
+        }
+
+document.getElementById("sendRequest").addEventListener("click", function() {
+    sendRequest();
+}, false);
