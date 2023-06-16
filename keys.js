194661cfa561996d873efcdff80f7a6415471e68
Andres Publio Gentile <a.gentile@correo.unimet.edu.ve>
2023-06-16 08:39:06 -0400

    feat: add gitignore

--- keys.js
+++ keys.js
@@ -1,2 +1,2 @@
-window.mykey = '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'
+window.mykey = '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'
 // window.mykey = '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'
